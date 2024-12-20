bb08436f0e37387b8d83127fafe72266dba841db
San3ch3g <117848931+San3ch3g@users.noreply.github.com>
2024-12-20 21:58:34 +0600

    last-dep

--- practica/practica/windows/payments/ReportWindow.xaml.cs
+++ practica/practica/windows/payments/ReportWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using pracrica.db;
 using pracrica.models;
+using System.Collections.Generic;
 
 namespace pracrica.windows.payments
 {
@@ -16,6 +18,7 @@ namespace pracrica.windows.payments
         public ReportWindow(DateTime? startDate, DateTime? endDate)
         {
             InitializeComponent();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             _dbService = new DatabaseService();
             _startDate = startDate;
             _endDate = endDate;
@@ -70,6 +73,63 @@ namespace pracrica.windows.payments
 
             // Привязать данные к ItemsControl
             ReportDataGrid.ItemsSource = categories;
+        }
+
+        // Метод для экспорта отчёта в Excel
+        private void ExportToExcel(List<ReportCategory> categories)
+        {
+            // Создаём имя файла с текущей датой
+            string fileName = $"Отчёт от {DateTime.Now:dd.MM.yyyy}.xlsx";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+
+            // Создаём новый Excel-файл
+            using (var package = new ExcelPackage())
+            {
+                foreach (var category in categories)
+                {
+                    // Создаём новый лист для каждой категории
+                    var worksheet = package.Workbook.Worksheets.Add(category.CategoryName);
+
+                    // Заголовки столбцов
+                    worksheet.Cells[1, 1].Value = "Название продукта";
+                    worksheet.Cells[1, 2].Value = "Итоговая сумма";
+
+                    // Заполняем данные
+                    int row = 2;
+                    foreach (var product in category.Products)
+                    {
+                        worksheet.Cells[row, 1].Value = product.ProductName;
+                        worksheet.Cells[row, 2].Value = product.TotalAmount;
+                        row++;
+                    }
+
+                    // Итоговая сумма категории
+                    worksheet.Cells[row, 1].Value = "Итого:";
+                    worksheet.Cells[row, 2].Value = category.TotalAmount;
+
+                    // Автоподбор ширины столбцов
+                    worksheet.Cells.AutoFitColumns();
+                }
+
+                // Сохраняем файл
+                package.SaveAs(new FileInfo(filePath));
+            }
+
+            MessageBox.Show($"Отчёт успешно экспортирован в файл: {filePath}");
+        }
+
+        // Обработчик для экспорта
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            var categories = ReportDataGrid.ItemsSource as List<ReportCategory>;
+            if (categories != null && categories.Any())
+            {
+                ExportToExcel(categories);
+            }
+            else
+            {
+                MessageBox.Show("Нет данных для экспорта.");
+            }
         }
     }
 }
